e16bf6d21ec85a3f68f7fe04a0af2de772f7847d
nahid <shams.nahid@ixorasolution.com>
2019-07-24 19:01:31 +0600

    initial commit

---
+++ src/main/java/io/agileintelligence/ppmtool/PpmToolApplication.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agileintelligence/ppmtool/PpmToolApplicationTests.java
@@ -0,0 +1,16 @@
+package io.agileintelligence.ppmtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
