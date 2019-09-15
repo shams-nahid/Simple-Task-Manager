ebb814de0dde6e858757698c9de69270f2c15b7b
nahid <shams.nahid@ixorasolution.com>
2019-09-15 16:12:18 +0600

    added react ui

---
+++ ui/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Dashboard from "./components/Dashboard";
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={ Dashboard } />
+        <Route exact path="/addProject" component={ AddProject } />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
